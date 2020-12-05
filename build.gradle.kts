253fbb64ce40b319ffdb0c9cd67f1e9fb8fc8044
Danilo Pianini <danilo.pianini@gmail.com>
2020-12-05 16:23:06 +0100

    Update project description

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,10 @@ dependencies {
 }
 
 publishOnCentral {
-    projectDescription.set(
+    projectDescription =
         "The GraphStream library. With GraphStream you deal with" +
         "graphs. Static and Dynamic. You create them from scratch, from a file" +
         "or any source. You display and render them. This is the core package" +
         "that contains the minimal needed to read and write a graph."
-    )
-
-    projectLongName.set("gs-core")
+    projectLongName = "gs-core"
 }
